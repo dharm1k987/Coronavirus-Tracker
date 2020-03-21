72f0d96aac6af232b72a2be37bbf8898f8dc34d4
Dharmik Shah <dharm1k987@gmail.com>
2020-03-21 19:30:23 -0400

    simplify code, concurrency broken

--- timeline/index.js
+++ timeline/index.js
@@ -1,87 +1,95 @@
-const fetch = require('node-fetch');
 const csv = require('@fast-csv/parse');
 const axios = require('axios');
 
-let data = []; 
-let dates = [];
-let countryToTotal = {};
+let data = []; // stores our rows
+let dates = []; // stores the dates in the header
+let countryToTotal = {}; // maps each country to an array which contains date to value mappings
 
-let types = ['Confirmed', 'Deaths', 'Recovered']
+let finalArrayConfirmed = []; // stores all the json objects to pass to backend
+let finalArrayDeaths = []; // stores all the json objects to pass to backend
+let finalArrayRecovered = []; // stores all the json objects to pass to backend
 
-async function main(type) {
-  // for (let i = 0; i < types.length; i++) {
-    // let type = types[i];
+async function helper(type) {
+    // type = Confirmed, Deaths or Recovered
     let url = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-${type}.csv`;
-    let res = await axios.get(url);
-    let res2 = await parseData(res.data, type.toLowerCase())
-    data = []
-    countryToTotal = {}
-  // }
-
+    let res = await axios.get(url); // res = content of csv file
+    await parseData(res.data, type.toLowerCase()) // WAIT for parsing to complete, we need to parse csv's separately
 }
 
-async function main2() {
-  await main('Confirmed')
-  data = [];
-  countryToTotal = {};
-  await main('Deaths')
-  data = [];
-  countryToTotal = {};
-  await main('Recovered')
+async function main() {
+  await helper('Confirmed'); // parse the Confirmed csv, WAIT for this to complete before moving on
+  data = []; countryToTotal = {}; // reset data structures
+  
+  await helper('Recovered'); // parse the Recovered csv, WAIT for this to complete before moving on
+  data = []; countryToTotal = {}; // reset data structures
+
+  await helper('Deaths'); // parse the Deaths csv
+
+  // await finalArrayConfirmed.forEach((country) => {
+  //     axios.post(`http://localhost:9000/timeline/update/confirmed`, {
+  //       newTimeline: finalArrayConfirmed[country]
+  //     })
+  // })
+
 }
 
 
 
 const parseData = (contents, type) => {
-  let allRows = contents.split('\n');
-  
-  // can do the following once
-  let row = allRows.map(r => r.trim().split(','))
-  if (dates.length == 0) {
-    for (let i = 4; i < row[0].length; i++) {
-      dates.push(row[0][i])
-    }
-  }
+ 
+  csv.parseString(contents)
+    .on('error', error => console.error(error))
+    .on('data', row => data.push(row))
+    .on('end', rowCount => {
+      // this adds the in the header to the array, only occurs once
+      if (dates.length == 0) {
+        for (let i = 4; i < data[0].length; i++) {
+          dates.push(data[0][i]);
+        }
+      }
 
-  let rowCount = row.length;
-  for (let i = 1; i < rowCount; i++) {
-    let country = row[i][1].toLowerCase();
-    
-    if (!(country in countryToTotal)) {
-      // setup inital mapping
-      countryToTotal[country] = dates.map(date => {
-        let obj = {};
-        obj[[date]] = 0;
-        return obj;
-      });
+      // all the rows are stored in data array, go through each row
+      // details are not important here
+      for (let i = 1; i < rowCount; i++) {
+        let country = data[i][1].toLowerCase();
 
-    }
-  
-    // console.log(dates)
+        // if we haven't mapped this country yet, then set up something like this:
+        // countryToTotal = { 'canada': [{'1/21': 0}, {'1/22': 0}, ...], ...}
+        if (!(country in countryToTotal)) {
+          countryToTotal[country] = dates.map(date => {
+            let obj = {};
+            obj[[date]] = 0;
+            return obj;
+          });  
+        }
 
-    for (let j = 0; j < dates.length; j++) {
-      let date = dates[j];
-      let toAddValue = parseInt(row[i][j + 4], 10);
+        // otherwise, figure out what to add, details not important
+        for (let j = 0; j < dates.length; j++) {
+          let date = dates[j];
+          let toAddValue = parseInt(data[i][j + 4], 10);  
+          countryToTotal[country][j][date] += toAddValue;
+        }
+      }
 
-      countryToTotal[country][j][date] += toAddValue;
-    }
-    let finalObj = {
-      country: country,
-      data: countryToTotal[country]
-    }
-    // console.log(finalObj)
+      if (type == 'Confirmed') finalArrayConfirmed.push(countryToTotal)
+      if (type == 'Deaths') finalArrayDeaths.push(countryToTotal)
+      if (type == 'Recovered') finalArrayRecovered.push(countryToTotal)
 
-    axios.post(`http://localhost:9000/timeline/update/${type}`, {
-      newTimeline: finalObj
-    }).catch(err => {
-      console.log("An error occured, email admins:")
-      console.log(err)
-      return
-    })
-    
-  }
+    });
 }
 
 
 
-main2();
+main();
+
+  // to fix:
+
+  // contents = contents.replace("Korea, South", 'South Korea')
+  // contents = contents.replace('Taiwan*', 'Taiwan')
+  // contents = contents.replace('Congo (Kinshasa)', 'Congo')
+  // contents = contents.replace("Cote d'Ivoire", 'Ivory Coast')
+  // contents = contents.replace("Congo (Brazzaville)", 'Ivory Coast')
+  // contents = contents.replace("Bahamas, The", 'Bahamas')
+  // contents = contents.replace("Gambia, The", 'Gambia') 
+
+  // contents = contents.replace("'", '')
