34c8f9b329bd2a6b2238b03ac2fbc3ef5a1c4fa7
Dharmik Shah <dharm1k987@gmail.com>
2020-03-21 10:45:05 -0400

    removed csv parser, instead do with for loop and results seem accurate now

--- timeline/index.js
+++ timeline/index.js
@@ -33,63 +33,53 @@ async function main2() {
 
 
 const parseData = (contents, type) => {
-  csv.parseString(contents)
-  .on('error', error => console.error(error))
-  .on('data', row => data.push(row))
-  .on('end', rowCount => {
-    if (dates.length == 0) {
-      for (let i = 4; i < data[0].length; i++) {
-        dates.push(data[0][i]);
-      }
+  let allRows = contents.split('\n');
+  
+  // can do the following once
+  let row = allRows.map(r => r.trim().split(','))
+  if (dates.length == 0) {
+    for (let i = 4; i < row[0].length; i++) {
+      dates.push(row[0][i])
     }
+  }
 
-
+  let rowCount = row.length;
+  for (let i = 1; i < rowCount; i++) {
+    let country = row[i][1].toLowerCase();
     
-    for (let i = 1; i < rowCount; i++) {
-      let country = data[i][1].toLowerCase();
+    if (!(country in countryToTotal)) {
+      // setup inital mapping
+      countryToTotal[country] = dates.map(date => {
+        let obj = {};
+        obj[[date]] = 0;
+        return obj;
+      });
 
-      if (!(country in countryToTotal)) {
-        // setup initial mapping
-        countryToTotal[country] = dates.map(date => {
-          let obj = {};
-          obj[[date]] = 0;
-          return obj;
-        });
+    }
+  
+    // console.log(dates)
 
-      }
-      // console.log(type, data[1])
-      // return
+    for (let j = 0; j < dates.length; j++) {
+      let date = dates[j];
+      let toAddValue = parseInt(row[i][j + 4], 10);
 
+      countryToTotal[country][j][date] += toAddValue;
+    }
+    let finalObj = {
+      country: country,
+      data: countryToTotal[country]
+    }
+    // console.log(finalObj)
 
-      for (let j = 0; j < dates.length; j++) {
-        let date = dates[j];
-        let toAddValue = parseInt(data[i][j + 4], 10);
-
-        countryToTotal[country][j][date] += toAddValue;
-      }
-      let finalObj = {
-        country: country,
-        data: countryToTotal[country]
-      }
-
-      fetch(`http://localhost:9000/timeline/update/${type}`, {
-          method: 'post',
-          body: JSON.stringify({
-            newTimeline: finalObj
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        })
-        .catch(err => {
-          console.log("An error occured, email admins:")
-          console.log(err)
-          return
-        })
-
-    }   
+    axios.post(`http://localhost:9000/timeline/update/${type}`, {
+      newTimeline: finalObj
+    }).catch(err => {
+      console.log("An error occured, email admins:")
+      console.log(err)
+      return
+    })
     
-  });
+  }
 }
 
 
