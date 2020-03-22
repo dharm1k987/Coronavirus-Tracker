17af899f655d5868f7454e908feb004a952c6274
Dharmik Shah <dharm1k987@gmail.com>
2020-03-21 20:41:50 -0400

    added promise.all, now have to send request, otherwise its good

--- timeline/index.js
+++ timeline/index.js
@@ -1,5 +1,5 @@
 const csv = require('@fast-csv/parse');
-const axios = require('axios');
+const axios = require('axios'); // can u type?
 
 let data = []; // stores our rows
 let dates = []; // stores the dates in the header
@@ -11,33 +11,54 @@ let finalArrayRecovered = []; // stores all the json objects to pass to backend
 
 async function helper(type) {
     // type = Confirmed, Deaths or Recovered
+    // console.log("helper called")
     let url = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-${type}.csv`;
     let res = await axios.get(url); // res = content of csv file
+    // console.log("res finished")
     await parseData(res.data, type.toLowerCase()) // WAIT for parsing to complete, we need to parse csv's separately
 }
 
 async function main() {
   await helper('Confirmed'); // parse the Confirmed csv, WAIT for this to complete before moving on
+  console.log(finalArrayConfirmed.length);
   data = []; countryToTotal = {}; // reset data structures
   
   await helper('Recovered'); // parse the Recovered csv, WAIT for this to complete before moving on
+  console.log(finalArrayRecovered.length);
   data = []; countryToTotal = {}; // reset data structures
 
   await helper('Deaths'); // parse the Deaths csv
+  console.log(finalArrayDeaths.length);
 
-  // await finalArrayConfirmed.forEach((country) => {
-  //     axios.post(`http://localhost:9000/timeline/update/confirmed`, {
-  //       newTimeline: finalArrayConfirmed[country]
-  //     })
+  await finalArrayConfirmed.forEach((c) => {
+    Object.keys(c).forEach(a => {
+      console.log(c[a])
+      return
+    })
+      // axios.post(`http://localhost:9000/timeline/update/confirmed`, {
+      //   newTimeline: finalArrayConfirmed[country]
+      // })
+  })
+
+  // await finalArrayRecovered.forEach((country) => {
+  //   axios.post(`http://localhost:9000/timeline/update/recovered`, {
+  //     newTimeline: finalArrayRecovered[country]
+  //   })
+  // })
+  // await finalArrayDeaths.forEach((country) => {
+  //   axios.post(`http://localhost:9000/timeline/update/deaths`, {
+  //     newTimeline: finalArrayDeaths[country]
+  //   })
   // })
 
 }
 
 
 
-const parseData = (contents, type) => {
+const parseData = async (contents, type) => {
  
-  csv.parseString(contents)
+  return new Promise(function(resolve, reject) {
+    csv.parseString(contents)
     .on('error', error => console.error(error))
     .on('data', row => data.push(row))
     .on('end', rowCount => {
@@ -71,11 +92,16 @@ const parseData = (contents, type) => {
         }
       }
 
-      if (type == 'Confirmed') finalArrayConfirmed.push(countryToTotal)
-      if (type == 'Deaths') finalArrayDeaths.push(countryToTotal)
-      if (type == 'Recovered') finalArrayRecovered.push(countryToTotal)
+      console.log(type)
+
+      if (type == 'confirmed') finalArrayConfirmed.push(countryToTotal)
+      if (type == 'deaths') finalArrayDeaths.push(countryToTotal)
+      if (type == 'recovered') finalArrayRecovered.push(countryToTotal)
+      resolve();
 
     });
+  })
+ 
 }
 
 
