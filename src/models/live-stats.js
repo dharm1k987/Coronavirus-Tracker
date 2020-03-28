3c31384172c4c7537b0bbb7d925307b805dd7c3a
Dharmik Shah <dharm1k987@gmail.com>
2020-03-28 18:32:22 -0400

    added function to do conversions

--- src/models/live-stats.js
+++ src/models/live-stats.js
@@ -15,26 +15,8 @@ const postStats = async (stats) => {
 
     stats[i].country = stats[i].country.toUpperCase();
 
-    if (stats[i].country == 'KOREA, SOUTH') stats[i].country = 'SOUTH KOREA';
-    if (stats[i].country == 'S. KOREA') stats[i].country = 'SOUTH KOREA';
-    if (stats[i].country == 'TAIWAN*') stats[i].country = 'TAIWAN';
-    if (stats[i].country == 'CONGO (KINSHASA)') stats[i].country = 'CONGO';
-    if (stats[i].country == "COTE D'IVOIRE") stats[i].country = 'IVORY COAST';
-    if (stats[i].country == 'CONGO (BRAZZAVILLE)') stats[i].country = 'CONGO';
-    if (stats[i].country == 'BAHAMAS, THE') stats[i].country = 'BAHAMAS';
-    if (stats[i].country == 'GAMBIA, THE') stats[i].country = 'GAMBIA';
-    if (stats[i].country == 'UK') stats[i].country = 'UNITED KINGDOM';
-    if (stats[i].country == 'US') stats[i].country = 'USA';
-    if (stats[i].country == 'CZECHIA') stats[i].country = 'CZECH REPUBLIC';
-    if (stats[i].country == 'GUINEABISSAU') stats[i].country = 'GUINEA-BISSAU';
-    if (stats[i].country == 'TIMORLESTE') stats[i].country = 'TIMOR-LESTE';
-    if (stats[i].country == 'CAR') stats[i].country = 'CENTRAL AFRICAN REPUBLIC';
-    if (stats[i].country.includes('DIAMOND')) stats[i].country = 'DIAMOND PRINCESS'
-    if (stats[i].country.includes('UNION')) stats[i].country = 'REUNION'
-    if (stats[i].country.includes('CURA')) stats[i].country = 'CURACAO'
-    if (stats[i].country.includes('ZAANDAM')) stats[i].country = 'MS ZAANDAM'
-    if (stats[i].country.includes('TOTAL:')) stats[i].country = 'TOTAL:'
-
+    stats[i].country = await countryReplace(stats[i].country)
+    
     const newStat = await Stat.updateOne(
       { country: stats[i].country },
       { ...stats[i] },
--- src/models/timeline.js
+++ src/models/timeline.js
@@ -37,25 +37,7 @@ const postTimeline = async (timeline, type) => {
   let updated = true;
   timeline.country = timeline.country.toUpperCase();
 
-  if (timeline.country == 'KOREA, SOUTH') timeline.country = 'SOUTH KOREA';
-  if (timeline.country == 'S. KOREA') timeline.country = 'SOUTH KOREA';
-  if (timeline.country == 'TAIWAN*') timeline.country = 'TAIWAN';
-  if (timeline.country == 'CONGO (KINSHASA)') timeline.country = 'CONGO';
-  if (timeline.country == "COTE D'IVOIRE") timeline.country = 'IVORY COAST';
-  if (timeline.country == 'CONGO (BRAZZAVILLE)') timeline.country = 'CONGO';
-  if (timeline.country == 'BAHAMAS, THE') timeline.country = 'BAHAMAS';
-  if (timeline.country == 'GAMBIA, THE') timeline.country = 'GAMBIA';
-  if (timeline.country == 'UK') timeline.country = 'UNITED KINGDOM';
-  if (timeline.country == 'US') timeline.country = 'USA';
-  if (timeline.country == 'CZECHIA') timeline.country = 'CZECH REPUBLIC';
-  if (timeline.country == 'GUINEABISSAU') timeline.country = 'GUINEA-BISSAU';
-  if (timeline.country == 'TIMORLESTE') timeline.country = 'TIMOR-LESTE';
-  if (timeline.country == 'CAR') timeline.country = 'CENTRAL AFRICAN REPUBLIC';
-  if (timeline.country.includes('DIAMOND')) timeline.country = 'DIAMOND PRINCESS'
-  if (timeline.country.includes('UNION')) timeline.country = 'REUNION'
-  if (timeline.country.includes('CURA')) timeline.country = 'CURACAO'
-  if (timeline.country.includes('ZAANDAM')) timeline.country = 'MS ZAANDAM'
-  if (timeline.country.includes('TOTAL:')) timeline.country = 'TOTAL:'
+  timeline.country = await countryReplace(timeline.country)
 
   const newTimeline = await Timeline.updateOne({ country: timeline.country, type: type }, { data: timeline.data, type: type },
      { upsert: true });
