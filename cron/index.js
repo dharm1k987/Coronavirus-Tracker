8eb5e0fc50eb89d139df8cb2f9dc09d02706f7a7
Dharmik Shah <dharm1k987@gmail.com>
2020-04-09 08:16:39 -0400

    found much easier way to parsing

--- cron/index.js
+++ cron/index.js
@@ -1,52 +1,42 @@
-const scrape = require('website-scraper');
-const options = {
-  urls: ['https://www.worldometers.info/coronavirus/'],
-  directory: './path',
-};
-
+const tabletojson = require('tabletojson').Tabletojson;
 const fetch = require('node-fetch');
-const fs = require('fs');
+const url = 'https://worldometers.info/coronavirus/';
 
 let objArray = [];
-scrape(options, (error, result) => {
 
-  fs.readFile('./path/index.html', function(err, data) {
-    let d = data.toString();
-    d = d.replace(/(\r\n|\n|\r)/gm, "");
-    let l = d.match(/<tr>(.*)<\/tr>/g); // match the <tr> .. </tr>
-    let s = l[0].split('</tr>');
+tabletojson.convertUrl(
+  url,
+  function(tablesAsJson) {
 
-    for (let i = 1; i < s.length - 1; i++) {
-
-      let p = s[i].match(/<td(.*)<\/td>/g); // find all the <td> tags
-      p[0] = p[0].replace(/<!--(.*?)-->/g, ' '); // remove the comments
-      let r = p[0].match(/>.*?<\/td>/g); // match between the <td> tags
-
-      y = r.map((r1) => {
-        let regex = "";
-        if (r1.includes('</a>')) {
-          // country case is special
-          regex = /.+(>.+)<\/a>/g;
-          r1 = regex.exec(r1)[1]
-        }
-        r1 = r1.replace(/\+|-|>|,|<\/td>/g, '');
-        r1 = r1.trim();
-        if (r1 == '') r1 = 0;
-        if (!isNaN(r1)) {
-          r1 = parseInt(r1)
-        }
-        return r1
-      })
+    // {
+    //   'Country,Other': 'San Marino',
+    //   TotalCases: '308',
+    //   NewCases: '+29',
+    //   TotalDeaths: '34',
+    //   NewDeaths: '',
+    //   TotalRecovered: '45',
+    //   ActiveCases: '229',
+    //   'Serious,Critical': '14',
+    //   'Tot Cases/1M pop': '9,077',
+    //   'Deaths/1M pop': '1,002',
+    //   TotalTests: '722',
+    //   'Tests/\n1M pop': '21,278',
+    //   Continent: 'Europe'
+    // }
+    
+    for (let i = 0; i < tablesAsJson[0].length; i++) {
+      let y = tablesAsJson[0][i];
 
       let obj = {
-        country: y[0],
-        totalCases: y[1],
-        newCases: y[2],
-        totalDeaths: y[3],
-        newDeaths: y[4],
-        totalRecovered: y[5],
-        activeCases: y[6],
-        seriousAndCritical: y[7]
+        country: y['Country,Other'],
+        totalCases: y['TotalCases'] ? parseInt(y['TotalCases'].replace(/\,/g, ''), 10) : 0,
+        newCases: y['NewCases'] ? parseInt(y['NewCases'].replace(/\+|\-|\,/g, ''), 10) : 0,
+        totalDeaths: y['TotalDeaths'] ? parseInt(y['TotalDeaths'].replace(/\,/g, ''), 10) : 0,
+        newDeaths: y['NewDeaths'] ? parseInt(y['NewDeaths'].replace(/\+|\-|\,/g, ''), 10) : 0,
+        totalRecovered: y['TotalRecovered'] ? parseInt(y['TotalRecovered'].replace(/\,/g, ''), 10) : 0,
+        activeCases: y['ActiveCases'] ? parseInt(y['ActiveCases'].replace(/\,/g, ''), 10) : 0,
+        seriousAndCritical: y['Serious,Critical'] ? parseInt(y['Serious,Critical'].replace(/\,/g, ''), 10) : 0,
+        continent: y['Continent']
       }
 
       if (obj.country.includes('Diamond Princess')) obj.country = 'Diamond Princess'
@@ -83,7 +73,5 @@ scrape(options, (error, result) => {
         console.log(err)
       })
 
-
-  })
-
-});
+  }
+);
